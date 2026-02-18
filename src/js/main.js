6f20b3ff622ae6008b7024bd7ea498f6a0f93745
Henrique Madureira <carloshenriquemadureira.santos@ucsal.edu.br>
2026-02-17 23:29:54 -0300

    feat(game): implement the game experience
    - add new sounds
    
    - implement pontuactions
    
    implement renderization and the api
    
    add images to readme

--- src/js/main.js
+++ src/js/main.js
@@ -1,53 +1,128 @@
-import { playSound } from './sound.js';
-import { correctQuestion } from './punctuation.js';
-import { punctuation } from './punctuation.js';
-import { reloadAnswers } from './apiData.js';
+import { playBackground, playSound } from "./sound.js";
+import {
+  correctQuestion,
+  percentPoints,
+  points,
+  punctuation,
+  wrongQuestion,
+  wrong,
+  reset,
+} from "./punctuation.js";
+import {
+  loadMockData,
+  data,
+  totalQuestions,
+  reloadAnswers,
+} from "./apiData.js";
+import { renderQuestionCard } from "./render.js";
 
-const currentPoints = document.querySelector('.result-card--value');
-const currentQuestions = document.querySelector('.stat-box--value-questions');
-currentQuestions.innerHTML = 10;
+let currentQuestionIndex = 0;
 
-const currentCorrectQuestions = document.querySelector('.stat-box--value-correct');
-currentCorrectQuestions.innerHTML = 8;
+const currentPercentValue = document.querySelector(".percent-value");
 
-const currentWrongQuestions = document.querySelector('.stat-box--value-wrong');
-currentWrongQuestions.innerHTML = 2;
-import { reloadAnswers } from "./apiData.js";
+const allScreens = document.querySelectorAll(".screen");
 
-const allScreens = document.querySelectorAll('.screen');
+const currentAnswerValue = document.querySelector(".score-card__hits");
 
-function goTo (idTarget) {
-    allScreens.forEach(screen => {
-        screen.classList.add('hidden');
-    });
-    
-    const screenTarget = document.getElementById(idTarget);
-    screenTarget.classList.remove('hidden');
+function disableSubmitButton() {
+  submitBtn.setAttribute("disabled", "");
 }
 
-const initialBtn = document.querySelector('.play-screen__btn-play');
-initialBtn.addEventListener('click', () => {
-    // reloadAnswers();
-    playSound('click');
-    goTo('winScreen');
+function enableSubmitButton() {
+  submitBtn.removeAttribute("disabled");
+}
+
+document.addEventListener("change", (event) => {
+  if (event.target.name === "answers") {
+    enableSubmitButton();
+  }
 });
 
-const gameBtn = document.querySelector('.game-controls__btn--sub');
-gameBtn.addEventListener('click', () => {
-    playSound('click');
-    goTo('winScreen');
-    goTo('playScreen');
+function goTo(idTarget) {
+  allScreens.forEach((screen) => {
+    screen.classList.add("hidden");
+  });
+
+  const screenTarget = document.getElementById(idTarget);
+  screenTarget.classList.remove("hidden");
+}
+
+const initialBtn = document.querySelector(".play-screen__btn-play");
+initialBtn.addEventListener("click", () => {
+  playBackground();
+  playSound("click");
+  // reloadAnswers();
+  loadMockData();
+  renderQuestionCard(data[currentQuestionIndex]);
+  goTo("gameScreen");
 });
 
-const homeBtn = document.querySelector('.result-card__return-btn');
-homeBtn.addEventListener('click', () => {
-    playSound('click');
-    goTo('playScreen');
+const submitBtn = document.querySelector(".game-controls__btn--sub");
+submitBtn.addEventListener("click", () => {
+  playSound("click");
+
+  const respostaSelecionada = document.querySelector(
+    'input[name="answers"]:checked',
+  );
+
+  if (!respostaSelecionada) {
+    alert("Hey! Select one option!");
+    return;
+  }
+
+  if (respostaSelecionada.value === "true") {
+    playSound("correct");
+    correctQuestion();
+    console.log("correct");
+  } else {
+    playSound("wrong");
+    wrongQuestion();
+    console.log("wrong");
+  }
+
+  currentPercentValue.innerHTML = percentPoints(points);
+
+  if (points > 1) {
+    currentAnswerValue.innerHTML = `You got ${points} answers corrects`;
+  } else {
+    currentAnswerValue.innerHTML = `You got ${points} answer correct`;
+  }
+
+  currentQuestionIndex++;
+
+  if (currentQuestionIndex < totalQuestions) {
+    renderQuestionCard(data[currentQuestionIndex]);
+    disableSubmitButton();
+  } else {
+    const currentQuestions = document.querySelector(
+      ".stat-box--value-questions",
+    );
+    currentQuestions.innerHTML = totalQuestions;
+
+    const currentCorrectQuestions = document.querySelector(
+      ".stat-box--value-correct",
+    );
+    currentCorrectQuestions.innerHTML = points;
+
+    const currentWrongQuestions = document.querySelector(
+      ".stat-box--value-wrong",
+    );
+    currentWrongQuestions.innerHTML = wrong;
+
+    const currentPoints = document.querySelector(".result-card--value");
+    currentPoints.innerHTML = punctuation(points);
+
+    currentQuestionIndex = 0;
+
+    playSound("finished");
+    playSound("finished_alt");
+    goTo("winScreen");
+  }
 });
 
-const btnTest = document.querySelector('.btn-test');
-btnTest.addEventListener('click', () => {
-    currentPoints.innerHTML = punctuation(correctQuestion());
-    goTo('playScreen');
+const resultBtn = document.querySelector(".result-card__return-btn");
+resultBtn.addEventListener("click", () => {
+  playSound("click");
+  reset();
+  goTo("playScreen");
 });
-
--- src/js/sound.js
+++ src/js/sound.js
@@ -1,13 +1,25 @@
 const sound = {
-    click: new Audio('./assets/sound/click.wav'),
-    win: new Audio('./assets/sound/click.wav'),
-    lose: new Audio('./assets/sound/click.wav'),
+  click: new Audio("./assets/sound/click.wav"),
+  correct: new Audio("./assets/sound/right_answer.wav"),
+  wrong: new Audio("./assets/sound/wrong_answer.wav"),
+  finished: new Audio("./assets/sound/finished.wav"),
+  finished_alt: new Audio("./assets/sound/finishing_alt.ogg"),
+  background: new Audio("./assets/sound/background.wav"),
+};
+
+export const playBackground = () => {
+  if (sound["background"]) {
+    sound["background"].currentTime = 0;
+    sound["background"].volume = 0.2;
+    sound["background"].loop = true;
+    sound["background"].play();
+  }
 };
 
 export const playSound = (name) => {
-    if (sound[name]) {
-        sound[name].currentTime = 0;
-        sound[name].volume = 1.0;
-        sound[name].play();
-    }
+  if (sound[name]) {
+    sound[name].currentTime = 0;
+    sound[name].volume = 1.0;
+    sound[name].play();
+  }
 };
